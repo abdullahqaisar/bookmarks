f6bfdbbc1568193961b7be4b5628b4b029fd3515
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2024-03-19 16:10:22 +0500

    Add bookmark feature

---
+++ src/bookmark/bookmark.controller.ts
@@ -0,0 +1,81 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard';
+import { BookmarkService } from './bookmark.service';
+import { GetUser } from 'src/auth/decorator';
+import {
+  CreateBookmarkDto,
+  EditBookmarkDto,
+} from './dto';
+
+@UseGuards(JwtGuard)
+@Controller('bookmarks')
+export class BookmarkController {
+  constructor(
+    private bookmarkService: BookmarkService,
+  ) {}
+  @Get()
+  getBookmarks(@GetUser('id') userId: number) {
+    return this.bookmarkService.getBookmarks(
+      userId,
+    );
+  }
+
+  @Get(':id')
+  getBookmarkbyId(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookmarkService.getBookmarkbyId(
+      userId,
+      bookmarkId,
+    );
+  }
+
+  @Post()
+  createBookmark(
+    @GetUser('id') userId: number,
+    @Body() dto: CreateBookmarkDto,
+  ) {
+    return this.bookmarkService.createBookmark(
+      userId,
+      dto,
+    );
+  }
+
+  @Patch(':id')
+  editBookmardById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+    @Body() dto: EditBookmarkDto,
+  ) {
+    return this.bookmarkService.editBookmardById(
+      userId,
+      bookmarkId,
+      dto,
+    );
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  deleteBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookmarkService.deleteBookmarkById(
+      userId,
+      bookmarkId,
+    );
+  }
+}
